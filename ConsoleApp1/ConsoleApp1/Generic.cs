b7ea8ecf87c1329035f3430dc8fc354dcc750f79
yukinasaito <saito@oysterworld.jp>
2019-10-18 19:03:45 +0900

    10/18

---
+++ ConsoleApp1/ConsoleApp1/Generic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Array<T>
+{
+    private T[] m_Items;
+    private void Set(int index, T item)
+    {
+        m_Items[index] = item;
+    }
+
+    public T Get(int index)
+    {
+        return m_Items[index];
+    }
+}
+
+//複数の型を定義できる
+public class Pair<T1, T2>
+{
+    public T1 m_Key;
+    public T2 m_Valie;
+}
+
+//whereを使って制約を設けることができる
+//IComparableインターフェースを実装するクラスだけがTに指定
+public static T Max<T>(T a, T b)where T : IComparable<T>
+{
+    if (0 < a.CompareTo(b))
+        return a;
+    else
+        return b;
+}
+
---
+++ ConsoleApp1/ConsoleApp1/LINQ.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication5 
+{
+    class Program
+	{
+        static void Main(string[] args)
+        {
+            List<TestResult> results = new List<TestResult>()
+            {
+                new TestResult(){ Name = "Suzuki Ichiro",Math = 80, Physics = 60},
+                new TestResult(){ Name = "Akagawa Jiro", Math = 70, Physics = 90},
+                new TestResult(){ Name = "Mikawa Saburo",Math = 90, Physics = 100},
+            };
+            //ここで、受験者全員の数学平均を求めたい
+         }       
+	}
+
+
+
+}
